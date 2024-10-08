fa21785df974e1c42231241c666ff0fe1c49df12
kellykkhoo <103315546+kellykkhoo@users.noreply.github.com>
2024-10-08 08:56:00 +0000

    update

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template,request
 import google.generativeai as genai
 import os
+from textblob import TextBlob
 
 api = os.getenv("MAKERSUITE_API_TOKEN") 
 genai.configure(api_key="AIzaSyCNYWXiaRU1zUpf6w08yrvHjMqaV77r3R8")
@@ -26,5 +27,17 @@ def makersuite():
 def predcition():
     return(render_template("predcition.html"))
 
+@app.route("/SA",methods=["GET","POST"])
+def SA():
+    return(render_template("SA.html"))
+
+@app.route("/SAR",methods=["GET","POST"])
+def SAR():
+    q = request.form.get("q")
+    r = TextBlob(q).sentiment
+    return(render_template("SAR.html", r=r))
+
 if __name__ == "__main__":
     app.run()
+
+
